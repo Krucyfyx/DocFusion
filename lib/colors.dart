e97e9bfe0aafdb0902a0cefb5db632e09074aecb
Krucyfyx <yugaanshgautam@gmail.com>
2023-08-08 23:29:36 +0530

    pushing code files to DocFusion repo

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kWhite = Colors.white;
+const kBlack = Colors.black;
+const kRed = Colors.red;
+const kBlue = Color.fromRGBO(26, 115, 232, 1);
+const kDocWhite = Color.fromRGBO(255, 255, 255, 1);
+
+var kGrey = Colors.grey.shade800;
---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+const host = 'http://192.168.157.233:3001';
+
+// ip address : 192.168.1.42
+// not public ip address
